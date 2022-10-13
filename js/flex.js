38ef615b3af39018e0ecd0b52aaef8f724b54afc
Aline Moicas <alinemoicas23@gmail.com>
2022-10-13 22:54:35 +0100

    posicionamentos objetos completo

--- js/flex.js
+++ js/flex.js
@@ -1,3 +1,15 @@
 function fd(valor){
     document.getElementById('container').style.flexDirection=valor;
 }
+
+function fw(valor){
+    document.getElementById('container').style.flexWrap=valor;
+}
+
+function fj(valor){
+    document.getElementById('container').style.justifyContent=valor;
+}
+
+function fat(valor){
+    document.getElementById('container').style.alignItems=valor;
+}
