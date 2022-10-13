c097c76103d96646572b2e584d92e1036067645f
Aline Moicas <alinemoicas23@gmail.com>
2022-10-13 23:25:35 +0100

    banner

--- js/flex.js
+++ js/flex.js
@@ -13,3 +13,17 @@ function fj(valor){
 function fat(valor){
     document.getElementById('container').style.alignItems=valor;
 }
+
+var n = 0;
+function slide(){
+
+    var imagens = ['../img/img1.jpg','../img/img2.png','../img/img2.jpg'];
+    var legendas = ['um homem alien','menina bonita','menina bonita'];
+    var linkspub= ['www.google.com','www.google.com','www.google.com'];
+    document.getElementById('image_pub').src=imagens[n];
+    document.getElementById('text_pub').innerHTML=legendas[n];
+    document.getElementById('link_pub').href=linkspub[n];
+    if(n<2) n++;
+    else n=0;
+    setTimeout("slide()",2000);
+}
